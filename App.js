fb200dcfab9942e2b7d7b441b173898298c87815
Suneel Kumar Khatri <khatrisuneel96@gmail.com>
2020-09-06 17:41:15 +0500

    screens

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Home from "./app/screens/Home";
+import Reviews from "./app/screens/Reviews";
+import About from "./app/screens/About";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Home />
+      <Reviews />
+      <About />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  // container: {
+  //   // flex: 1,
+  //   backgroundColor: "#fff",
+  // },
 });
