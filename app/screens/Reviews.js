ead0f8e8e8333c551e6c027c8e6e759a0e8e860d
Suneel Kumar Khatri <khatrisuneel96@gmail.com>
2020-09-06 22:12:48 +0500

    Global styles

--- app/screens/Reviews.js
+++ app/screens/Reviews.js
@@ -1,16 +1,11 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
+import { globalStyles } from "../assets/styles/Global";
 
 export default function Reviews() {
   return (
-    <View style={styles.container}>
+    <View style={globalStyles.container}>
       <Text>Review</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 25,
-  },
-});
