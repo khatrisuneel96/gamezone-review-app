f93823216c90d5f6bdb638a9cc68572966825201
Suneel Kumar Khatri <khatrisuneel96@gmail.com>
2020-09-07 00:46:03 +0500

    Moving between screens

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
 import { globalStyles } from "../assets/styles/Global";
 
-export default function Home() {
+export default function Home({ navigation }) {
+  const pressHandler = () => {
+    navigation.navigate("Review Details");
+  };
+
   return (
     <View style={globalStyles.container}>
       <Text style={globalStyles.textItem}>Home Screen Test</Text>
+      <Button title="Go to Reviews" onPress={pressHandler} />
     </View>
   );
 }
